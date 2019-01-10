4add16f3c5fd599128129b5ce11aecbce37aacd1
danielpapajohn <dpapajohn@hotmail.com>
2019-01-09 21:18:27 -0800

    Add project files.

---
+++ BannedScrapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BannedScrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
